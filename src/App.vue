b4b53184b7a171c5c3cc84eb1806bb12e48c8cb9
omonovsayidsharifxon <omonovsayidsharifxon@gmail.com>
2025-01-06 21:24:03 +0500

    crud-project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "./components/Header.vue";
+import PostList from "./components/PostList"
+</script>
+
+<template>
+  <main class="main-body">
+    <div class="container">
+      <Header />
+      <PostList/>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.main-body{
+  background-color: #eff1f4;
+}
+.container {
+  width: 80%;
+  margin: auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="posts-header">
+        <h1 class="posts-heading">Posts</h1>
+        <button class="add-post-btn">+ New post</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.posts-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.posts-heading{
+    color: #343940;
+}
+.add-post-btn{
+    background-color: #5180FD;
+    color: #fff;
+    border-radius: 16px;
+    padding: 12px 16px;
+    height: 48px;
+    cursor: pointer;
+    border:none
+}
+
+</style>
